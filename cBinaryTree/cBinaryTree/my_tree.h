8f5dc6139c9ba4a3ceb6477f6af7de2382c6bc8f
Aakash Chitroda <aakash.chitroda@hotmail.com>
2020-03-28 13:52:38 -0700

    First commit of cBinaryTree

--- cBinaryTree/cBinaryTree/my_tree.h
+++ cBinaryTree/cBinaryTree/my_tree.h
@@ -10,5 +10,18 @@
 #define my_tree_h
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+struct _TreeNode {
+    int32_t val;
+    struct _TreeNode *left;
+    struct _TreeNode *right;
+};
+
+typedef struct _Treenode TreeNode;
+
+TreeNode *my_tree__create_new_treeNode(int val);
+TreeNode *my_tree__insert_treeNode(TreeNode *node, int val);
 #endif /* my_tree_h */
