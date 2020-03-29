14629891397ad35118cbc2b175267f14901fe277
Aakash Chitroda <aakash.chitroda@hotmail.com>
2020-03-29 00:16:04 -0700

    Added function to find inorder successor, delete a node, has path sum

--- cBinaryTree/cBinaryTree/my_tree.c
+++ cBinaryTree/cBinaryTree/my_tree.c
@@ -47,7 +47,7 @@ int32_t my_tree__get_size(TreeNode *node) {
         return (my_tree__get_size(node->left) + 1 + my_tree__get_size(node->right));
 }
 
-// Do the stack trace using online python tool.. Google it
+// Do the stack trace using online tool named "pythontutor"..Google it
 int32_t my_tree__get_max_depth(TreeNode *node) {
     if (NULL == node) return 0;
     int depth_at_left = my_tree__get_max_depth(node->left);
@@ -130,8 +130,11 @@ void my_tree__print_postorder_recurrsive(TreeNode *node) {
 /*
  Also,called as Breadth-First Search (BFS)
  This can be solved using Queue.
- In C++, there is STL called Queue that can be used.
- But, in C, we need to implement Queue for TreeNode
+ In C++, there is a STL called Queue that can be used.
+ But, in C, we need to implement Queue for TreeNode.
+ My Queue implementation is in my_queue.c. Please note
+ it may not be the perfect implementation and may need some
+ more edge cases.
  */
 void my_tree__print_level_order(TreeNode *node) {
     queue *q = NULL;
@@ -173,4 +176,97 @@ static bool my_tree__is_bst_helper_function(TreeNode *node, int min_range, int m
 
 bool my_tree__is_bst_efficient(TreeNode *node) {
     return my_tree__is_bst_helper_function(node, INT32_MIN, INT32_MAX);
+}
+
+static TreeNode *my_tree__get_min_value_node(TreeNode *node) {
+    if (NULL == node) return node;
+    while (node->left != NULL) node = node->left;
+    return node;
+}
+
+static TreeNode *my_tree__get_max_value_node(TreeNode *node) {
+    if (NULL == node) return node;
+    while (node->right != NULL) node = node->right;
+    return node;
+}
+
+TreeNode *my_tree__delete_node(TreeNode *node, int target) {
+    if (NULL == node) return node;
+    else if (target < node->val) node->left = my_tree__delete_node(node->left, target);
+    else if (target > node->val) node->right = my_tree__delete_node(node->right, target);
+    else {
+        // Case 1: No child
+        if ((NULL == node->left) && (NULL == node->right)) {
+            TreeNode *node_to_delete = node;
+            free(node_to_delete);
+            node_to_delete = NULL;
+        }
+        // Case 2A: If only right child is there
+        else if (NULL == node->left) {
+            TreeNode *node_to_delete = node;
+            node = node->right;
+            free(node_to_delete);
+            node_to_delete = NULL;
+        }
+        // Case 2B: If only left child is present
+        else if (NULL == node->right) {
+            TreeNode *node_to_delete = node;
+            node = node->left;
+            free(node_to_delete);
+            node_to_delete = NULL;
+        }
+        /* Case 3: Both left and right child are present
+        * 1.   Find min_node in right subtree
+        *      Copy the value in the targetted node
+        *      Delete duplicate from right subtree
+        *               OR
+        * 2.   Find max_node in left subtree
+        *      Copy the value in the targetted node
+        *      Delete duplicate from right subtree
+        */
+        else {
+            TreeNode *min_node = my_tree__get_min_value_node(node->right);
+            node->val = min_node->val;
+            node->right = my_tree__delete_node(node->right, min_node->val);
+        }
+    }
+    return node;
+}
+
+static TreeNode *my_tree__lookup_node(TreeNode *node, int target) {
+    if (NULL == node) return NULL;
+    if (target < node->val) return my_tree__lookup_node(node->left, target);
+    else if (target > node->val) return my_tree__lookup_node(node->right, target);
+    else return node;
+}
+
+TreeNode *my_tree__get_inorder_successor(TreeNode *node, int32_t successor_of_this_value) {
+    // Get the node for which we are finding the successor node
+    TreeNode *current = my_tree__lookup_node(node, successor_of_this_value);
+    if (NULL == current) return NULL;
+    // Case 1: If node has right subtree
+    if (current->right != NULL) {
+        return my_tree__get_min_value_node(current->right);
+    }
+    // Case 2: If node doesn't have right subtree
+    else {
+        TreeNode *successor = NULL;
+        TreeNode *ancestor = node;
+        while (ancestor != current) {
+            if (current->val < ancestor->val) {
+                successor = ancestor;
+                ancestor = ancestor->left;
+            } else {
+                ancestor = ancestor->right;
+            }
+        }
+        return successor;
+    }
+}
+
+bool my_tree__has_path_sum(TreeNode *node, int sum) {
+    if (NULL == node) return 0 == sum;
+    sum = sum - node->val;
+    return (my_tree__has_path_sum(node->left, sum) ||
+            my_tree__has_path_sum(node->right, sum));
 }
--- cBinaryTree/cBinaryTree/my_tree.h
+++ cBinaryTree/cBinaryTree/my_tree.h
@@ -20,9 +20,10 @@ typedef struct _TreeNode {
     struct _TreeNode *right;
 } TreeNode;
 
-
 TreeNode *my_tree__create_new_treeNode(int32_t val);
+
 TreeNode *my_tree__insert_treeNode(TreeNode *node, int32_t val);
+
 bool my_tree__lookup(TreeNode *node, int32_t target);
 
 int32_t my_tree__get_size(TreeNode *node);
@@ -48,4 +49,11 @@ void my_tree__print_level_order(TreeNode *node);
 bool my_tree__is_bst_not_efficient(TreeNode *node);
 
 bool my_tree__is_bst_efficient(TreeNode *node);
+
+TreeNode *my_tree__delete_node(TreeNode *node, int target);
+
+TreeNode *my_tree__get_inorder_successor(TreeNode *node, int32_t successor_of_this_value);
+
+bool my_tree__has_path_sum(TreeNode *node, int sum);
+
 #endif /* my_tree_h */
