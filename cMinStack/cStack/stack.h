d0a67f7d27117105e9f9f21bd5e3d89aa966142d
Aakash Chitroda <aakash.chitroda@hotmail.com>
2020-04-11 17:43:22 -0700

    Code modularization and code renaming

--- cMinStack/cStack/stack.h
+++ cMinStack/cStack/stack.h
@@ -19,26 +19,30 @@ typedef struct _stack_node{
     int32_t stack_pointer;
     int32_t stack_capacity;
     int32_t stack_size;
-} stack_node_t;
+} stack_node__t;
 
-typedef struct _min_stack_t{
-    stack_node_t *main_stack, *min_stack;
-} min_stack_t;
+typedef struct {
+    stack_node__t *main_stack, *min_stack, *max_stack;
+} stack__t;
 
 /** initialize your data structure here. */
 
-min_stack_t* min_stack__create(void);
+stack__t* stack__create(void);
 
-void min_stack__push(min_stack_t* obj, int32_t val);
+void stack__push(stack__t* obj, int32_t val);
 
-void min_stack__pop(min_stack_t* obj);
+void stack__pop(stack__t* obj);
 
-int32_t min_stack__top(min_stack_t* obj);
+int32_t stack__top(stack__t* obj);
 
-int32_t min_stack__get_min(min_stack_t* obj);
+int32_t stack__get_min(stack__t* obj);
 
-void min_stack__free(min_stack_t* obj);
+int32_t stack__get_max(stack__t* obj);
 
+bool stack__is_empty(stack__t* obj);
 
+bool stack__is_full(stack__t* obj);
+
+void stack__free(stack__t* obj);
 
 #endif /* min_stack_h */
